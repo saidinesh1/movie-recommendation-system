6e2db30609f50e6bf493919c163033d191702154
sai dinesh <saidineshbb24@gmail.com>
2024-01-19 09:58:16 +0530

    added background

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,9 +1,11 @@
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
+import Modal from 'react-modal';
+import { useState } from 'react';
 export const Dashboard = () => {
   const navigate = useNavigate();
   const { currentUser, logOut } = useAuth();
+  const [modalOpen,setModalOpen]=useState(false)
   const handleLogOut = async () => {
     await logOut().then(() => {
       navigate('/login');
@@ -14,7 +16,8 @@ export const Dashboard = () => {
       {currentUser.email}
       {JSON.stringify(currentUser)}
       <img src={currentUser.photoURL} alt='icon' />
-      <button onClick={handleLogOut}>Sign Out</button>
+      <button onClick={()=>{setModalOpen(true)}}>Sign Out</button>
+      <Modal isOpen={modalOpen} className={'h-[200px] w-[200px] '}><button onClick={handleLogOut}>Are you sure want to signout ?</button></Modal>
     </div>
   );
 };
--- src/pages/log-in.jsx
+++ src/pages/log-in.jsx
@@ -45,13 +45,13 @@ export const LogIn = () => {
   };
 
   return (
-    <section className='bg-gray-50 dark:bg-gray-900'>
-      
+    <section className='bg-gray-50 dark:bg-gray-900' style={{ backgroundImage:`url(https://wallpapers.com/images/high/aesthetic-anime-background-fwccitdecpa1jglo.webp)`,backgroundPosition:'cover',backgroundRepeat:'no-repeat',width: '100vw',
+    height: '100vh' }}>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
         <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
           <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
             <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-              Login in to your account
+              Login to your account
             </h1>
             <form className='space-y-4 md:space-y-6'>
               <div>
