fd3bddc7a596246d8852bd01f670b62fbe68e567
Sai Dinesh B <sai@foxsense.io>
2024-01-26 20:55:24 +0530

    added icons for tabs

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,7 +1,7 @@
 export const Tabs = ({ tabOptions, selectedTab, onSelect, children }) => {
   return (
     <div className='flex flex-row'>
-      <div className='border-b border-gray-200 dark:border-gray-700'>
+      <div className=' dark:border-gray-700'>
         <ul className='flex flex-col -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400'>
           {tabOptions.map((option) => (
             <li
@@ -11,25 +11,13 @@ export const Tabs = ({ tabOptions, selectedTab, onSelect, children }) => {
               <button
                 key={option.label}
                 aria-current='page'
-                className={`inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg group ${
+                className={`inline-flex items-center justify-center p-4 rounded-t-lg group text-[24px] ${
                   selectedTab === option
                     ? 'text-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group'
                     : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300'
                 }`}
               >
-                <svg
-                  className={`w-4 h-4 me-2 ${
-                    selectedTab === option
-                      ? 'text-blue-600  rounded-t-lg active dark:text-blue-500 dark:border-blue-500'
-                      : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300'
-                  }`}
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                >
-                  <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
-                </svg>
+                {option.icon ? option.icon : null}
                 {option.label}
               </button>
             </li>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal';
 import { Tabs } from '../components/Tab';
+import backgroundImage from '../assets/dashboard-bg.jpg';
+import bgImage from '../assets/dashboard-bg.jpg';
 import { posterFetch } from '../api/poster';
 import { tabOptions } from '../constants/dashboard.constant';
 import { useAuth } from '../contexts/AuthContext';
@@ -12,9 +14,10 @@ export const Dashboard = () => {
   const { currentUser, logOut } = useAuth();
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedTab, setSelectedTab] = useState(tabOptions[2]);
+  const [posters, setPosters] = useState();
   const handlePosterFetch = async () => {
     try {
-      const response = await posterFetch('Titanic');
+      const response = await posterFetch(input);
 
       if (!response.ok) {
         throw new Error('Failed to fetch movie data');
@@ -23,8 +26,10 @@ export const Dashboard = () => {
       const data = await response.json();
 
       if (data.results && data.results.length > 0) {
+        setPosters(data.results);
         const posterPath = data.results[0].poster_path;
         const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+
         console.log('Movie Poster URL:', posterUrl);
       } else {
         throw new Error('No results found');
@@ -33,15 +38,16 @@ export const Dashboard = () => {
       console.error('Error fetching movie poster:', error.message);
     }
   };
+  console.log(posters, 'posters');
   const handleLogOut = async () => {
     await logOut().then(() => {
       navigate('/login');
     });
   };
   const onSelect = (option) => {
-    console.log(option);
     setSelectedTab(option);
   };
+
   return (
     <div>
       <Tabs
@@ -50,7 +56,7 @@ export const Dashboard = () => {
         onSelect={onSelect}
         children={
           selectedTab.label === 'Search' ? (
-            <div>
+            <div className='flex flex-col'>
               <input
                 className='border border-2'
                 type='text'
@@ -62,6 +68,20 @@ export const Dashboard = () => {
               <button className='border border-2' onClick={handlePosterFetch}>
                 Search
               </button>
+              <div className='flex flex-row gap-x-[10px]'>
+                {posters &&
+                  posters.length > 0 &&
+                  posters.map(
+                    (poster) =>
+                      poster.poster_path && (
+                        <img
+                          src={`https://image.tmdb.org/t/p/w500${poster.poster_path}`}
+                          alt='icon'
+                          className='w-[100px] h-[100px]'
+                        />
+                      )
+                  )}
+              </div>
             </div>
           ) : (
             <></>
